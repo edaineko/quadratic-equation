4f1ef41b10a9981b2ec1700d8a3a0a303143add5
edaineko <daineko.ekaterina@gmail.com>
2018-02-26 17:45:21 +0300

    quadratic-equation solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.replace(/\s/g,'');
+    var  arr = equation.split('x');
+
+    var a = parseInt(arr[0]),
+        b = parseInt(arr[1].slice(2)),
+        c = parseInt(arr[2]),
+        d = b * b - 4 * a * c;
+
+    if (d > 0) {
+        var solutions = [];
+        var x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+        var x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+        solutions.push(x1,x2);
+
+        return solutions.sort(function (f, g) {
+            return f - g;
+        });
+
+    } else {
+        return 0;
+    }
 }
